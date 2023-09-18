cf5e78b0d7cec8007ec4b5eac4f3367739b8f11d
Manjinder Sandhu <sandhums@gmail.com>
2023-09-18 18:01:49 +0530

    initial2

--- Sources/HISSharedDTO/RegisterResponseDTO.swift
+++ Sources/HISSharedDTO/RegisterResponseDTO.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
